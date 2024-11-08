3e6797931a7c51d8ba121cd9e077934c8972f23e
thejondaw <mrjondaw@gmail.com>
2024-11-08 16:26:57 -0600

    eks #3

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -122,7 +122,7 @@ resource "aws_cloudwatch_log_group" "eks" {
 # ==================== EKS Cluster ==================== #
 
 resource "aws_eks_cluster" "study" {
-  name     = "study-cluster-с"
+  name     = "studycluster"
   role_arn = aws_iam_role.eks_cluster.arn
   version  = "1.28"
 
@@ -144,7 +144,7 @@ resource "aws_eks_cluster" "study" {
   ]
 
   tags = {
-    Name        = "study-cluster-с"
+    Name        = "studycluster"
     Environment = "study"
   }
 }
