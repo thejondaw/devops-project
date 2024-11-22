45534c53678cdd3c795db904cae9946fa410a426
thejondaw <mrjondaw@gmail.com>
2024-11-22 14:21:12 -0600

    11/22/24 #5

--- terraform/modules/tools/outputs.tf
+++ terraform/modules/tools/outputs.tf
@@ -2,10 +2,10 @@
 # ================= OUTPUTS OF TOOLS ================= #
 # ==================================================== #
 
-# Output - Created Namespaces
-output "namespaces" {
-  description = "Created namespace names"
-  value       = [for ns in kubernetes_namespace.applications : ns.metadata[0].name]
-}
+# # Output - Created Namespaces
+# output "namespaces" {
+#   description = "Created namespace names"
+#   value       = [for ns in kubernetes_namespace.applications : ns.metadata[0].name]
+# }
 
 # ==================================================== #
