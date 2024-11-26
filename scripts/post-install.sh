fe9530da0e272d137bf0852cf260e045d6c5d643
thejondaw <mrjondaw@gmail.com>
2024-11-25 22:45:25 -0600

    11/25/24 #6

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -8,9 +8,6 @@ if [ -z "$CLUSTER_NAME" ]; then
 fi
 aws eks update-kubeconfig --name $CLUSTER_NAME --region us-east-2
 
-# EBS CSI Driver - Пока нахуй не надо, попробую через ArgoCD
-# kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=master"
-
 # Build Helm dependencies
 cd helm/charts/aws-ebs-csi-driver && helm dependency build && cd ../../..
 cd helm/charts/ingress-nginx && helm dependency build && cd ../../..
@@ -26,12 +23,9 @@ kubectl apply -f k8s/infrastructure/network-policies.yaml
 kubectl apply -f k8s/argocd/applications/develop/aws-ebs-csi-driver.yaml
 kubectl apply -f k8s/argocd/applications/develop/ingress-nginx.yaml
 kubectl apply -f k8s/argocd/applications/develop/vault.yaml
+kubectl apply -f k8s/argocd/applications/develop/monitoring.yaml
+
+# kubectl apply -f k8s/argocd/applications/develop/logging.yaml
 # kubectl apply -f k8s/argocd/applications/develop/api.yaml
 # kubectl apply -f k8s/argocd/applications/develop/web.yaml
-# kubectl apply -f k8s/argocd/applications/develop/monitoring.yaml
-# kubectl apply -f k8s/argocd/applications/develop/logging.yaml
 # kubectl apply -f k8s/argocd/applications/develop/apparmor.yaml
-
-# Find DB Name & Patch 
-# DB_ENDPOINT=$(aws rds describe-db-instances --query 'DBInstances[0].Endpoint.Address' --output text)
-# kubectl patch configmap api-cm -n develop -p "{\"data\":{\"DB_HOST\":\"$DB_ENDPOINT\"}}"
