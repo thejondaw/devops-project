db760397c92c73ef0750003ba6e850228ef2ad4c
thejondaw <mrjondaw@gmail.com>
2024-11-26 16:56:09 -0600

    11/26/24 #11

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -21,10 +21,10 @@ kubectl apply -f k8s/infrastructure/namespaces.yaml
 kubectl apply -f k8s/infrastructure/network-policies.yaml
 
 # Install Applications via ArgoCD
-# kubectl apply -f k8s/argocd/applications/develop/aws-ebs-csi-driver.yaml
-# kubectl apply -f k8s/argocd/applications/develop/ingress-nginx.yaml
-# kubectl apply -f k8s/argocd/applications/develop/vault.yaml
-# kubectl apply -f k8s/argocd/applications/develop/monitoring.yaml
+kubectl apply -f k8s/argocd/applications/develop/aws-ebs-csi-driver.yaml
+kubectl apply -f k8s/argocd/applications/develop/ingress-nginx.yaml
+kubectl apply -f k8s/argocd/applications/develop/vault.yaml
+kubectl apply -f k8s/argocd/applications/develop/monitoring.yaml
 kubectl apply -f k8s/argocd/applications/develop/loki.yaml
 kubectl apply -f k8s/argocd/applications/develop/promtail.yaml
 
