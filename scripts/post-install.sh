82991703ae0e12ff839a7eebfad1510222d221e0
thejondaw <mrjondaw@gmail.com>
2024-11-26 19:14:35 -0600

    11/26/24 #13

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -13,8 +13,6 @@ cd helm/charts/aws-ebs-csi-driver && helm dependency build && cd ../../..
 cd helm/charts/ingress-nginx && helm dependency build && cd ../../..
 cd helm/charts/vault && helm dependency build && cd ../../..
 cd helm/charts/monitoring && helm dependency build && cd ../../..
-cd helm/charts/loki && helm dependency build && cd ../../..
-cd helm/charts/promtail && helm dependency build && cd ../../..
 
 # Create Infrastructure
 kubectl apply -f k8s/infrastructure/namespaces.yaml
@@ -25,8 +23,6 @@ kubectl apply -f k8s/argocd/applications/develop/aws-ebs-csi-driver.yaml
 kubectl apply -f k8s/argocd/applications/develop/ingress-nginx.yaml
 kubectl apply -f k8s/argocd/applications/develop/vault.yaml
 kubectl apply -f k8s/argocd/applications/develop/monitoring.yaml
-kubectl apply -f k8s/argocd/applications/develop/loki.yaml
-kubectl apply -f k8s/argocd/applications/develop/promtail.yaml
 
 # kubectl apply -f k8s/argocd/applications/develop/api.yaml
 # kubectl apply -f k8s/argocd/applications/develop/web.yaml
